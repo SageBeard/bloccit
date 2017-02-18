03edd46fb8a1eea469c3c5ce7788191c9b70cd31
Sage Beard <sagek711@gmail.com>
2017-02-18 08:00:47 -0800

    assn 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/faq'
+
   get 'welcome/index'
 
   get 'welcome/about'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/faq'
 
   root 'welcome#index'
 
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,19 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  describe "GET index" do
-  it "renders the index template" do
-# #2
-    get :index
-# #3
-    expect(response).to render_template("index")
-  end
-end
 
-   describe "GET about" do
-     it "renders the about template" do
-       get :about
-       expect(response).to render_template("about")
-     end
-   end  
+  describe "GET #faq" do
+    it "returns http success" do
+      get :faq
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
