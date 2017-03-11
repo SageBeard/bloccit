3d09dd4d33a0956cebc3b0adf0a0f3166a4210f8
Sage Beard <sagek711@gmail.com>
2017-03-11 12:21:11 -0800

    chkpt 19 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -12,4 +13,28 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def new
+# #7
+  @post = Post.new
+  end
+
+  def create
+# #9
+  @post = Post.new
+  @post.title = params[:post][:title]
+  @post.body = params[:post][:body]
+
+# #10
+  if @post.save
+# #11
+    flash[:notice] = "Post was saved."
+    redirect_to @post
+  else
+# #12
+    flash.now[:alert] = "There was an error saving the post. Please try again."
+    render :new
+  end
+end
+
 end
