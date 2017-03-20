3a0655d37a040d613dd57caa74dc68dc0cd20664
Sage Beard <sagek711@gmail.com>
2017-03-19 20:59:43 -0700

    chkpt 21 complete

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ end
 group :development, :test do
    gem 'rspec-rails', '~> 3.0'
    gem 'pry-rails', :group => :development
+   gem 'shoulda'
  end
 
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
 require 'random_data'
 
-# Create Posts
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
-# #1
   Post.create!(
-# #2
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-# Create Comments
-# #3
+
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
   )
@@ -26,5 +30,6 @@ Post.find_or_create_by(title: 'Penelope', body: 'Unique body')
 puts "#{Post.count} posts created"
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
