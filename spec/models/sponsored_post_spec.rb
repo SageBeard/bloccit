d3568ab80a191c8f8ebf0c064b2c304dd2cef87e
Sage Beard <sagek711@gmail.com>
2017-03-23 18:51:10 -0700

    sponsored posts model

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 RSpec.describe SponsoredPost, type: :model do
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
-  let(:price) { RandomData.random_number }
+  let(:price) { RandomData.random_word }
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:sponsored_post) { topic.sponsored_posts.create!(title: title, body: body, price: price) }
 
   it { is_expected.to belong_to(:topic) }
   describe "attributes" do
