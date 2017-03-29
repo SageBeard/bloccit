654aa3310b93f2d8c31cd59b36362bad4568b50b
Sage Beard <sagek711@gmail.com>
2017-03-28 19:51:20 -0700

    chkpt 26 complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :topics do
   resources :posts, except: [:index]
   resources :sponsored_posts, except: [:index]
+
 end
 
 resources :users, only: [:new, :create]
+resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
