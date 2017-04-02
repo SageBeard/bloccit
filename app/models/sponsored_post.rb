fc1b89d6f762cedea78cad53b01c403f6884d20a
Sage Beard <sagek711@gmail.com>
2017-04-02 16:54:11 -0700

    chkpt 27 complete

--- app/models/sponsored_post.rb
+++
@@ -1,4 +0,0 @@
-class SponsoredPost < ActiveRecord::Base
-  belongs_to :topic
-  has_many :comments
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+# Create Users
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
 
 15.times do
   Topic.create!(
@@ -10,6 +20,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -34,11 +45,18 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'youremail.com', # replace this with your personal email
+  password: 'helloworld'
+)
+
 puts "#{Post.count} posts created"
 Post.find_or_create_by(title: 'Penelope', body: 'Unique body')
 puts "#{Post.count} posts created"
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} posts created"
