86a2371fbb38337d3f4f72ab419c86c2367dcb0b
Sage Beard <sagek711@gmail.com>
2017-04-04 21:57:17 -0700

    attempt 1

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -60,10 +60,10 @@ class TopicsController < ApplicationController
       params.require(:topic).permit(:name, :description, :public)
     end
 
-  def authorize_user
-    unless current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
+    def authorize_user
+      unless current_user.admin? || current_user.moderator?
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to topics_path
+      end
     end
-  end
  end
