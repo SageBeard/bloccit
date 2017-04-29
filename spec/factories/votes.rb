a972ee45444970a38db3aaaeceb2b0918a8651fb
Sage Beard <sagek711@gmail.com>
2017-04-28 19:56:21 -0700

    value issue

---
+++ spec/factories/votes.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :vote
+    value 0
+    post
+    user
+end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
-  let(:post) { create(:post) } 
+  let(:post) { create(:post) }
+  let(:vote) { create(:vote)}
 
   # #1
    it { is_expected.to belong_to(:post) }
